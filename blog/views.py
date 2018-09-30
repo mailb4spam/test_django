b0f306c367b194b70d3e84afa1745baf7c723786
mailb4spam <mailb4spam@gmail.com>
2018-09-30 10:04:52 +0300

    Change the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def post_list(request):
+	return render(request, 'blog/post_list.html',{})
+
+def index(request):
+    return HttpResponse('Hello, welcome to the index page.')
+
+def individual_post(request):
+    return HttpResponse('Hi, this is where an individual post will be.')
+
